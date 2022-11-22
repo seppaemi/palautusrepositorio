5d60d2c3be2cd9371f972917be6e244415f0df98
seppaemi <emilia.seppala@helsinki.fi>
2022-11-22 23:18:13 +0200

    5.5. kaksi tuotetta lisättyä korin hinta tavaroiden hintojen summa test

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -27,3 +27,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(porkkana)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_eri_tuotteen_lisattya_korin_hinta_tuotteiden_hitnojen_summa(self):
+        maito = Tuote("Maito", 3)
+        porkkana = Tuote("Porkkana", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(porkkana)
+        self.assertEqual(self.kori.hinta(), 8)
